224a7e000b9ce73cfe0ae39a6589fe05a66df013
Andressa Da Costa <andressadacostan@gmail.com>
2022-07-08 15:54:30 -0300

    add hello-world

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+// import React from 'react';
+import { render } from "react-dom";
+import { App } from './App'
 
-function App() {
-  return <h1>Hello World</h1>
-}
+render(<App />, document.getElementById('root'))
 
-const user = {
-  name:'Andressa',
-}
-console.log(user.address?.street)
+
+
+// render(<h1>Test</h1>, document.getElementById('root'))
+ 
+// import React from 'react'
+// import { App } from './App'
+
+// function App() {
+//   return <h1>Hello World</h1>
+// }
+
+// const user = {
+//   name:'Andressa',
+// }
+// console.log(user.address?.street)
